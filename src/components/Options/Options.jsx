3240acd7170dbce0436f24b96ec630e09fa70516
pab-mich <pab.michelini@gmail.com>
2023-06-26 15:23:56 +0100

    improvent navbar functionality

---
+++ src/components/Options/Options.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Options.css";
+
+const Options = () => {
+  return (
+    <section className='options-container'>
+      <h2>Empeza gratis</h2>
+      <div className='options-buttons-container'>
+        <button className='aboutUs-button'>Become a Driver</button>
+        <button className='aboutUs-button'>Become a Passenger</button>
+      </div>
+    </section>
+  );
+};
+
+export default Options;
